6a9f3d53174621dc0a4c6145afccc5694c99239e
FpRaArNkK <mspark1184@naver.com>
2024-06-20 14:53:25 +0900

    [Setting/#1] 프로젝트 init

---
+++ UniVoice/UniVoice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniVoice
+//
+//  Created by 박민서 on 6/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
