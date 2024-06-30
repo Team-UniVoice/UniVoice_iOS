0ccc01443c2808c1711d9d44ade544550904b2ad
FpRaArNkK <mspark1184@naver.com>
2024-07-01 03:05:30 +0900

    [Feat/#5] InitialView 작성

--- UniVoice/UniVoice/Presentation/LoginPage/View/InitialView.swift
+++ UniVoice/UniVoice/Presentation/LoginPage/View/InitialView.swift
@@ -12,6 +12,11 @@ import Then
 final class InitialView: UIView {
     
     // MARK: Views
+    private let logoImageView = UIImageView()
+    let buttonStack = UIStackView()
+    let startButton = UIButton()
+    let loginButton = UIButton()
+    let councilButton = UIButton()
     
     // MARK: Init
     override init(frame: CGRect) {
@@ -34,18 +39,55 @@ final class InitialView: UIView {
     // MARK: setUpHierarchy
     private func setUpHierarchy() {
         [
-
+            startButton,
+            loginButton,
+            councilButton
+        ].forEach { buttonStack.addArrangedSubview($0) }
+        
+        [
+            logoImageView,
+            buttonStack
         ].forEach { self.addSubview($0) }
     }
     
     // MARK: setUpUI
     private func setUpUI() {
-
+        logoImageView.do {
+            $0.image = UIImage(systemName: "graduationcap.fill")
+        }
+        
+        buttonStack.do {
+            $0.axis = .vertical
+            $0.spacing = 10
+        }
+        
+        startButton.do {
+            $0.setTitle("유니보이스 시작하기", for: .normal)
+            $0.setTitleColor(.black, for: .normal)
+        }
+        
+        loginButton.do {
+            $0.setTitle("로그인 하기", for: .normal)
+            $0.setTitleColor(.black, for: .normal)
+        }
+        
+        councilButton.do {
+            $0.setTitle("학생회이신가요?", for: .normal)
+            $0.setTitleColor(.black, for: .normal)
+        }
     }
     
     // MARK: setUpLayout
     private func setUpLayout() {
-
+        logoImageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.size.equalTo(200)
+        }
+        
+        buttonStack.snp.makeConstraints {
+            $0.bottom.equalTo(self.safeAreaLayoutGuide).inset(20)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+        }
     }
 }
 
