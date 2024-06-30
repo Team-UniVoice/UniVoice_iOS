7c7b0bdae8262baea0fbbe3fe696bdf9e5583817
FpRaArNkK <mspark1184@naver.com>
2024-07-01 02:00:52 +0900

    [Feat/#5] LoginView 작성

--- UniVoice/UniVoice/Presentation/LoginPage/View/LoginView.swift
+++ UniVoice/UniVoice/Presentation/LoginPage/View/LoginView.swift
@@ -6,10 +6,16 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 final class LoginView: UIView {
     
     // MARK: Views
+    let idTextField = UITextField()
+    let pwTextField = UITextField()
+    let loginButton = UIButton()
+    
     // MARK: Init
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -25,18 +31,54 @@ final class LoginView: UIView {
     
     // MARK: setUpFoundation
     private func setUpFoundation() {
-        
+        self.backgroundColor = .white
     }
+    
     // MARK: setUpHierarchy
     private func setUpHierarchy() {
-        
+        [
+            idTextField,
+            pwTextField,
+            loginButton
+        ].forEach { self.addSubview($0) }
     }
+    
     // MARK: setUpUI
     private func setUpUI() {
+        idTextField.do {
+            $0.placeholder = "아이디"
+        }
         
+        pwTextField.do {
+            $0.placeholder = "비밀번호"
+        }
+        
+        loginButton.do {
+            $0.setTitle("확인", for: .normal)
+            $0.backgroundColor = .gray
+        }
     }
+    
     // MARK: setUpLayout
     private func setUpLayout() {
+        idTextField.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide)
+            $0.centerX.equalToSuperview()
+        }
         
+        pwTextField.snp.makeConstraints {
+            $0.top.equalTo(idTextField.snp.bottom).offset(20)
+            $0.centerX.equalToSuperview()
+        }
+        
+        loginButton.snp.makeConstraints {
+            $0.bottom.equalTo(self.safeAreaLayoutGuide).inset(20)
+            $0.centerX.equalToSuperview()
+        }
     }
+}
+
+@available(iOS 17.0, *)
+#Preview {
+    LoginView()
 }
