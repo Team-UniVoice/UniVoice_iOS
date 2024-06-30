9a21b37454fbaaf7012a71246ce056051966179b
FpRaArNkK <mspark1184@naver.com>
2024-07-01 01:51:16 +0900

    [Feat/#5] LoginView init

---
+++ UniVoice/UniVoice/Presentation/LoginPage/View/LoginView.swift
@@ -0,0 +1,42 @@
+//
+//  LoginView.swift
+//  UniVoice
+//
+//  Created by 박민서 on 7/1/24.
+//
+
+import UIKit
+
+final class LoginView: UIView {
+    
+    // MARK: Views
+    // MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpFoundation()
+        setUpHierarchy()
+        setUpUI()
+        setUpLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: setUpFoundation
+    private func setUpFoundation() {
+        
+    }
+    // MARK: setUpHierarchy
+    private func setUpHierarchy() {
+        
+    }
+    // MARK: setUpUI
+    private func setUpUI() {
+        
+    }
+    // MARK: setUpLayout
+    private func setUpLayout() {
+        
+    }
+}
